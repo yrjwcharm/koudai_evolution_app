21f5b7511e2a8c3c6f7fa4694b99a9fa5ad34cfa
邓修 <dengxiu@licaimofang.com>
2021-04-23 15:31:35 +0800

    fixed

--- src/pages/Vision/ArticleDetail.js
+++ src/pages/Vision/ArticleDetail.js
@@ -2,7 +2,7 @@
  * @Date: 2021-03-18 10:57:45
  * @Author: dx
  * @LastEditors: dx
- * @LastEditTime: 2021-04-22 22:32:16
+ * @LastEditTime: 2021-04-23 15:31:16
  * @Description: 文章详情
  */
 import React, {useCallback, useEffect, useRef, useState} from 'react';
@@ -233,7 +233,9 @@ const ArticleDetail = ({navigation, route}) => {
                         ref={webviewRef}
                         scalesPageToFit={Platform.select({ios: true, android: false})}
                         source={{
-                            uri: `${BaseUrl.H5}/article/${route.params?.article_id}`,
+                            uri: isArticle.current
+                                ? `${BaseUrl.H5}/article/${route.params?.article_id}`
+                                : `${BaseUrl.H5}${route.params?.link}`,
                         }}
                         startInLoadingState
                         style={{height: webviewHeight}}
