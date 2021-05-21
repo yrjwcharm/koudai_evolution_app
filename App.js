d320dddd0c0b6eac086be454dd6cc9b2e32a883b
yanhongchang <y_hongchang@163.com>
2021-05-21 10:33:30 +0800

    fix

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * @Date: 2020-11-03 19:28:28
  * @Author: yhc
  * @LastEditors: yhc
- * @LastEditTime: 2021-05-19 16:21:55
+ * @LastEditTime: 2021-05-21 10:32:52
  * @Description: app全局入口文件
  */
 import 'react-native-gesture-handler';
@@ -14,7 +14,6 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './src/routes';
 import configStore from './src/redux';
-import CodePush from 'react-native-code-push';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RootSiblingParent} from 'react-native-root-siblings';
 import * as WeChat from 'react-native-wechat-lib';
@@ -37,62 +36,17 @@ if (Platform.OS === 'android') {
         UIManager.setLayoutAnimationEnabledExperimental(true);
     }
 }
-const MyTheme = {
-    dark: true,
-    colors: {
-        primary: 'rgb(0, 0, 0)',
-        background: 'rgb(0, 255, 255)',
-        card: 'rgb(255, 255, 255)',
-        text: 'rgb(255, 0, 255)',
-        border: 'rgb(199, 199, 204)',
-        notification: 'rgb(255, 69, 58)',
-    },
-};
+
 const {store, persistor} = configStore();
-let codePushOptions = {
-    //设置检查更新的频率
-    //ON_APP_RESUME APP恢复到前台的时候
-    //ON_APP_START APP开启的时候
-    //MANUAL 手动检查
-    checkFrequency: CodePush.CheckFrequency.MANUAL,
-};
+
 function App(props) {
-    // const scheme = useColorScheme();
     const navigationRef = useRef();
     const routeNameRef = useRef();
     const [modalObj, setModalObj] = React.useState({});
     const [userInfo, setUserInfo] = React.useState({});
     const imageH = useRef(0);
     const homeShowModal = useRef(true);
-    //如果有更新的提示
-    const syncImmediate = () => {
-        CodePush.sync({
-            //安装模式
-            //ON_NEXT_RESUME 下次恢复到前台时
-            //ON_NEXT_RESTART 下一次重启时
-            //IMMEDIATE 马上更新
-            mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
-            //对话框
-            updateDialog: {
-                //是否显示更新描述
-                appendReleaseDescription: true,
-                //更新描述的前缀。 默认为"Description"
-                descriptionPrefix: '更新内容：',
-                //强制更新按钮文字，默认为continue
-                mandatoryContinueButtonLabel: '立即更新',
-                //强制更新时的信息. 默认为"An update is available that must be installed."
-                mandatoryUpdateMessage: '必须更新后才能使用',
-                //非强制更新时，按钮文字,默认为"ignore"
-                optionalIgnoreButtonLabel: '稍后',
-                //非强制更新时，确认按钮文字. 默认为"Install"
-                optionalInstallButtonLabel: '后台更新',
-                //非强制更新时，检查到更新的消息文本
-                optionalUpdateMessage: '有新版本了，是否更新？',
-                //Alert窗口的标题
-                title: '更新提示',
-            },
-        });
-    };
+
     let lastBackPressed = '';
     const onBackAndroid = () => {
         if (lastBackPressed && lastBackPressed + 2000 >= Date.now()) {
@@ -486,4 +440,4 @@ function App(props) {
     );
 }
 
-export default CodePush(codePushOptions)(App);
+export default App;
