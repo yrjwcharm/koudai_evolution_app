33cd4df023bf6df0d610e8ef543cdc3cf80b5670
LL <liule@licaimofang.com>
2021-05-27 20:11:38 +0800

    安卓热更新

--- android/app/src/main/java/com/licaimofang/app/MainApplication.java
+++ android/app/src/main/java/com/licaimofang/app/MainApplication.java
@@ -3,104 +3,111 @@
  * @Author: yhc
  * @LastEditors: yhc
  * @LastEditTime: 2021-05-26 11:13:13
- * @Description: 
+ * @Description:
  */
 
 package com.licaimofang.app;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
+import com.theweflex.react.WeChatPackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import cn.jiguang.plugins.push.JPushModule; //jpush
-import com.theweflex.react.WeChatPackage; //微信
-import com.microsoft.codepush.react.CodePush; //热更新
+
+import cn.jiguang.plugins.push.JPushModule;
+
 // import com.github.wumke.RNExitApp.RNExitAppPackage;
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+            return BuildConfig.DEBUG;
         }
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-           packages.add(new WeChatPackage());
-          new CodePush(
-         "umln5OVCBk6nTjd37apOaHJDa71g4ksvOXqog",
-         MainApplication.this,
-         BuildConfig.DEBUG,
-         "https://cps.licaimofang.com/" 
-      );
-          //  packages.add(new RNExitAppPackage());
-          return packages;
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            packages.add(new WeChatPackage());
+
+            for (ReactPackage reactPackage : packages) {
+                if (reactPackage instanceof CodePush) {
+                    CodePush.getJSBundleFile();
+                    new CodePush(
+                            "umln5OVCBk6nTjd37apOaHJDa71g4ksvOXqog",
+                            MainApplication.this,
+                            BuildConfig.DEBUG,
+                            "https://cps.licaimofang.com/");
+                }
+            }
+            //  packages.add(new RNExitAppPackage());
+            return packages;
         }
+
         @Override
         protected String getJSMainModuleName() {
-          return "index";
+            return "index";
         }
+
         @Override
         protected String getJSBundleFile() {
-         return CodePush.getJSBundleFile();
+            return CodePush.getJSBundleFile();
         }
-      };
+    };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     //调用此方法：点击通知让应用从后台切到前台
-    JPushModule.registerActivityLifecycle(this);
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+        //调用此方法：点击通知让应用从后台切到前台
+        JPushModule.registerActivityLifecycle(this);
+    }
 
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
+    /**
+     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+     *
+     * @param context
+     * @param reactInstanceManager
+     */
+    private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+        if (BuildConfig.DEBUG) {
+            try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.licaimofang.app.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
+                Class<?> aClass = Class.forName("com.licaimofang.app.ReactNativeFlipper");
+                aClass
+                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                        .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
 }
