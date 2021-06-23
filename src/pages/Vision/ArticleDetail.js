6e62ebfdc6582a3ff26a0e6e6203b9631395ae3c
yanhongchang <y_hongchang@163.com>
2021-06-23 18:31:00 +0800

    文章fix

--- src/pages/Vision/ArticleDetail.js
+++ src/pages/Vision/ArticleDetail.js
@@ -2,7 +2,7 @@
  * @Date: 2021-03-18 10:57:45
  * @Author: dx
  * @LastEditors: yhc
- * @LastEditTime: 2021-06-21 11:47:14
+ * @LastEditTime: 2021-06-23 18:29:55
  * @Description: 文章详情
  */
 import React, {useCallback, useEffect, useRef, useState} from 'react';
@@ -87,8 +87,10 @@ const ArticleDetail = ({navigation, route}) => {
                         })
                     );
                 }
+                if (eventData * 1) {
+                    setHeight(eventData * 1 || deviceHeight);
+                }
             }
-            setHeight(eventData * 1 || deviceHeight);
         }
     };
     const onFavor = useCallback(
