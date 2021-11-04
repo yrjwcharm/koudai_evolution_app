df43a5947e4cc4538d4d28c80a318d9b65de714b
yanhongchang <y_hongchang@163.com>
2021-11-04 10:46:54 +0800

    修改ios im域名

--- src/pages/IM/im.js
+++ src/pages/IM/im.js
@@ -2,7 +2,7 @@
  * @Date: 2021-01-12 21:35:23
  * @Author: yhc
  * @LastEditors: yhc
- * @LastEditTime: 2021-11-04 10:38:10
+ * @LastEditTime: 2021-11-04 10:46:25
  * @Description:
  */
 import React, {useState, useEffect, useRef} from 'react';
@@ -133,7 +133,7 @@ const IM = (props) => {
         //建立WebSocket连接
         WS.current.onopen = function () {
             http.get(
-                Platform.OS == 'android'
+                Platform.OS == 'android' && SERVER_URL[global.env].IMApiSsl
                     ? `${SERVER_URL[global.env].IMApiSsl}/im/token`
                     : `${SERVER_URL[global.env].IMApi}/im/token`,
                 {entry: props.route.params?.entry}
