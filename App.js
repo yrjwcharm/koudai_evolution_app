f35acd2fab644de45340fd1e44e6c636a336fb4e
yanhongchang <y_hongchang@163.com>
2021-12-01 15:59:08 +0800

    fix

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * @Date: 2020-11-03 19:28:28
  * @Author: yhc
  * @LastEditors: yhc
- * @LastEditTime: 2021-11-26 14:52:40
+ * @LastEditTime: 2021-12-01 15:58:17
  * @Description: app全局入口文件
  */
 import 'react-native-gesture-handler';
@@ -129,7 +129,7 @@ function App(props) {
             if (res && res.refresh_token) {
                 var ts = new Date().getTime();
                 if (ts > res.expires_at * 1000) {
-                    http.post('/auth/user/token_refresh/20210101', {refresh_token: res.refresh_token}).then((data) => {
+                    http.post('/auth/user/refresh_token/20210101', {refresh_token: res.refresh_token}).then((data) => {
                         Storage.save('loginStatus', data.result);
                     });
                 }
