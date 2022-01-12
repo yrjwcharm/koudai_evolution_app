4e59117e44f6adbc80794d94da6f116aa729450b
dengxiu <dengxiu@licaimofang.com>
2022-01-12 15:52:49 +0800

    fixed

--- src/pages/Activity/ReportWebView.js
+++ src/pages/Activity/ReportWebView.js
@@ -2,7 +2,7 @@
  * @Date: 2021-03-19 11:23:44
  * @Author: yhc
  * @LastEditors: dx
- * @LastEditTime: 2022-01-12 11:14:44
+ * @LastEditTime: 2022-01-12 15:51:54
  * @Description:年报
  */
 import React, {useCallback, useEffect, useRef, useState} from 'react';
@@ -167,7 +167,7 @@ export default function WebView({route, navigation}) {
                                         transform: [{scale: 1}],
                                     },
                                 }}
-                                duration={2000}
+                                duration={1500}
                                 onAnimationEnd={() => envelopeRef.current?.setNativeProps({style: {zIndex: 0}})}
                                 ref={envelopeRef}
                                 source={require('../../assets/img/envelope.png')}
