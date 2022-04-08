c184644d42ca618a67e63380935449277a3d729f
yanhongchang-1105 <72793045+yanhongchang-1105@users.noreply.github.com>
2022-04-08 17:07:12 +0800

    v622

--- src/pages/Assets/index.js
+++ src/pages/Assets/index.js
@@ -2,7 +2,7 @@
  * @Date: 2020-12-23 16:39:50
  * @Author: yhc
  * @LastEditors: yhc
- * @LastEditTime: 2022-04-08 16:39:50
+ * @LastEditTime: 2022-04-08 16:45:32
  * @Description: 我的资产页
  */
 import React, {useState, useEffect, useRef, useCallback} from 'react';
