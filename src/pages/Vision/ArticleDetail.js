7d0123e293b73d2090641967824b0f575c0e1953
wxp <wangxuepeng@licaimofang.com>
2022-04-15 11:51:43 +0800

    弹窗增加判断

--- src/pages/Vision/ArticleDetail.js
+++ src/pages/Vision/ArticleDetail.js
@@ -245,24 +245,26 @@ const ArticleDetail = ({navigation, route}) => {
             });
         }
         // 提示弹窗
-        http.get('/community/article/popup/20220406', {
-            article_id: route.params?.article_id,
-            done_status: data?.read_info?.done_status || +finishRead,
-            article_progress: progress,
-        }).then((res) => {
-            if (res.code === '000000') {
-                const result = res.result;
-                result &&
-                    Modal.show({
-                        title: result.title,
-                        content: result.content,
-                        confirmText: result.confirm?.text,
-                        confirmCallBack: () => {
-                            jump(result.confirm?.url);
-                        },
-                    });
-            }
-        });
+        if (route?.params?.type !== 5) {
+            http.get('/community/article/popup/20220406', {
+                article_id: route.params?.article_id,
+                done_status: data?.read_info?.done_status || +finishRead,
+                article_progress: progress,
+            }).then((res) => {
+                if (res.code === '000000') {
+                    const result = res.result;
+                    result &&
+                        Modal.show({
+                            title: result.title,
+                            content: result.content,
+                            confirmText: result.confirm?.text,
+                            confirmCallBack: () => {
+                                jump(result.confirm?.url);
+                            },
+                        });
+                }
+            });
+        }
     }, [data, finishRead, headerHeight, postProgress, route, scrollY, webviewHeight, fr, jump]);
     // 刷新一下
     const refreshNetWork = useCallback(() => {
