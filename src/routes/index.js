2e59f3e118a2212fd3278dffd44d1415a4d9dba2
dengxiu <dengxiu@licaimofang.com>
2022-04-21 16:15:20 +0800

    PR-735 风险揭示书

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2020-12-23 16:39:50
  * @Author: yhc
- * @LastEditors: yhc
- * @LastEditTime: 2022-04-18 10:48:01
+ * @LastEditors: dx
+ * @LastEditTime: 2022-04-21 10:57:55
  * @Description:路由表
  */
 import React from 'react';
@@ -182,6 +182,7 @@ import RationalRecord from '../pages/Vision/RationalRecord'; // 理性值记录
 import RationalUpgrade from '../pages/Vision/RationalUpgrade'; // 理性等级升级
 import CommentList from '../pages/Portfolio/CommentList'; // 评论列表
 import PublishComment from '../pages/Portfolio/PublishComment'; // 发布评论
+import RiskDisclosure from '../pages/Assets/RiskDisclosure'; // 风险揭示书
 const Stack = createStackNavigator();
 
 export default function AppStack() {
@@ -719,6 +720,7 @@ export default function AppStack() {
             <Stack.Screen name="RationalUpgrade" component={RationalUpgrade} options={{title: ''}} />
             <Stack.Screen name="CommentList" component={CommentList} options={{title: ''}} />
             <Stack.Screen name="PublishComment" component={PublishComment} options={{title: ''}} />
+            <Stack.Screen name="RiskDisclosure" component={RiskDisclosure} options={{title: ''}} />
         </Stack.Navigator>
     );
 }
