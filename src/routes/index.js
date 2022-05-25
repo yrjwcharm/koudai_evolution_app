1e08ca452d84bf169c00a67ac5c80fa99760f600
dengxiu <dengxiu@licaimofang.com>
2022-05-25 18:25:36 +0800

    PR-761 title

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
  * @Date: 2020-12-23 16:39:50
  * @Author: yhc
  * @LastEditors: dx
- * @LastEditTime: 2022-05-24 17:00:28
+ * @LastEditTime: 2022-05-25 18:25:23
  * @Description:路由表
  */
 import React from 'react';
@@ -541,7 +541,7 @@ export default function AppStack() {
             <Stack.Screen name="ComplaintsAdvices" component={ComplaintsAdvices} options={{title: '投诉建议'}} />
             <Stack.Screen name="MessageBoard" component={MessageBoard} options={{title: '用户留言详情'}} />
             <Stack.Screen name="MessageNotice" component={MessageNotice} options={{title: ''}} />
-            <Stack.Screen name="PrivateRedeem" component={PrivateRedeem} options={{title: '赎回流程'}} />
+            <Stack.Screen name="PrivateRedeem" component={PrivateRedeem} options={{title: ''}} />
             <Stack.Screen name="PrivateApply" component={PrivateApply} options={{title: ''}} />
             <Stack.Screen name="DetailPolaris" component={DetailPolaris} options={{title: ''}} />
             <Stack.Screen name="StrategyPolaris" component={StrategyPolaris} options={{title: ''}} />
