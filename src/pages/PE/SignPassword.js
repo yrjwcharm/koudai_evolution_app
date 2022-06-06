ccfddfb3c8bb5fc5f1b3b424917d11170b4f6e48
yanhongchang-1105 <72793045+yanhongchang-1105@users.noreply.github.com>
2022-06-06 16:20:43 +0800

    fix

---
+++ src/pages/PE/SignPassword.js
@@ -0,0 +1,21 @@
+/*
+ * @Date: 2022-06-06 16:18:56
+ * @Author: yhc
+ * @LastEditors: yhc
+ * @LastEditTime: 2022-06-06 16:19:43
+ * @Description:设置签署密码
+ */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SignPassword = () => {
+    return (
+        <View>
+            <Text>SignPassword</Text>
+        </View>
+    );
+};
+
+export default SignPassword;
+
+const styles = StyleSheet.create({});
