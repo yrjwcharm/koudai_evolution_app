1342527eda7c23719c4a93b20c2d2503e6022bee
yanhongchang-1105 <72793045+yanhongchang-1105@users.noreply.github.com>
2022-07-11 11:49:57 +0800

    tmp

---
+++ src/pages/Assets/Asset/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2022-07-11 11:41:32
+ * @Description:我的资产新版
+ */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    );
+};
+export default Index;
+const styles = StyleSheet.create({});
