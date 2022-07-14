671d3510e530a8323b88fa976d32c7a0da7227c0
wxp <wangxuepeng@licaimofang.com>
2022-07-14 14:45:06 +0800

    sync

--- src/pages/PK/pages/Compare/PKManagerInfo.js
+++ src/pages/PK/pages/Compare/PKManagerInfo.js
@@ -151,7 +151,7 @@ const ValuePart = React.memo(({item}) => {
                     <Text
                         style={[
                             styles.valueText,
-                            {color: obj?.yield > 0 ? '#E74949' : obj?.yield === 0 ? '' : '#4BA471'},
+                            {color: obj?.yield > 0 ? '#E74949' : obj?.yield === 0 ? '#121D3A' : '#4BA471'},
                         ]}>
                         {obj?.yield + '%'}
                     </Text>
@@ -215,7 +215,6 @@ const styles = StyleSheet.create({
     valueText: {
         fontSize: px(14),
         lineHeight: px(22),
-        color: '#121D3A',
         fontFamily: Font.numFontFamily,
         textAlign: 'center',
     },
