b7921a02f463ff488ae2c2aea1f8d0c376c2344c
yanhongchang-1105 <72793045+yanhongchang-1105@users.noreply.github.com>
2022-07-15 11:06:23 +0800

    fix

--- src/pages/Attention/Index/index.js
+++ src/pages/Attention/Index/index.js
@@ -2,7 +2,7 @@
  * @Date: 2022-06-21 14:16:13
  * @Author: yhc
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-07-15 10:34:25
+ * @LastEditTime: 2022-07-15 11:06:14
  * @Description:关注
  */
 import {StyleSheet, View, Animated} from 'react-native';
@@ -45,7 +45,6 @@ const Attention = () => {
         setActiveTab(0);
     }, [is_login]);
     useEffect(() => {
-        console.log(data, '123');
         data?.follow && getFollowData({item_type: data?.follow?.tabs[activeTab].item_type});
     }, [activeTab, data]);
     const onChangeTab = (obj) => {
