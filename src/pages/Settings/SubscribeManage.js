98995f6fcf62f418e0ab61f5206c80704a8fadaf
wxp <wangxuepeng@licaimofang.com>
2022-07-21 15:02:39 +0800

    订阅管理

--- src/pages/Settings/SubscribeManage.js
+++ src/pages/Settings/SubscribeManage.js
@@ -1,78 +1,113 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Switch} from 'react-native';
+import React, {useEffect, useMemo, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Switch, ScrollView} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {px} from '~/utils/appUtil';
-import * as WeChat from 'react-native-wechat-lib';
-import Toast from '~/components/Toast';
-import Clipboard from '@react-native-community/clipboard';
+import http from '~/services';
+import {useJump} from '~/components/hooks';
 
 const SubscribeManage = () => {
-    const copyToFollow = () => {
-        Clipboard.setString('123');
-        WeChat.isWXAppInstalled().then((isInstalled) => {
-            if (isInstalled) {
-                try {
-                    WeChat.openWXApp();
-                } catch (e) {
-                    if (e instanceof WeChat.WechatError) {
-                        console.error(e.stack);
-                    } else {
-                        throw e;
-                    }
+    const jump = useJump();
+    const [data, setData] = useState(null);
+    const [status, setStatus] = useState([]);
+
+    const getData = () => {
+        http.get('/project/get/subscribe/conf/202207').then((res) => {
+            if (res.code === '000000') {
+                setData(res.result);
+                setStatus(
+                    res.result?.list?.reduce((memo, item) => {
+                        memo[item.item_id] = item.status === 'ON';
+                        return memo;
+                    }, {})
+                );
+            }
+        });
+    };
+    useEffect(() => {
+        getData();
+    }, []);
+
+    useEffect(() => {
+        let arr = Object.values(status).slice(1);
+        if (arr.length) {
+            let num = arr.reduce((memo, cur) => (cur ? ++memo : memo), 0);
+            let id = data.list[0].item_id;
+            let state = num === arr.length;
+            if (state !== status[id]) setStatus((val) => ({...val, [id]: state}));
+        }
+    }, [status, data]);
+
+    const handlerCellSwitch = (state, id, idx) => {
+        if (idx === 0) id = data.list.map((item) => item.item_id)?.join();
+        http.post('/project/set/subscribe/conf/202207', {status: state ? 'ON' : 'OFF', item_id: id}).then((res) => {
+            if (res.code === '000000') {
+                if (idx === 0) {
+                    setStatus((val) => {
+                        let newVal = {...val};
+                        for (let key in newVal) {
+                            newVal[key] = state;
+                        }
+                        return newVal;
+                    });
+                } else {
+                    setStatus((val) => ({...val, [id]: state}));
                 }
-            } else {
-                Toast.show('请安装微信');
             }
         });
     };
 
-    const handlerCellSwitch = (val) => {
-        console.log(val);
-    };
-
-    return (
+    return data ? (
         <View style={styles.container}>
             <View style={styles.topHintWrap}>
-                <Text style={styles.topHintText}>可通过APP和微信公众号查看买卖信号提醒消息</Text>
+                <Text style={styles.topHintText}>{data.header.line1}</Text>
             </View>
             <View style={styles.copyCell}>
-                <FastImage
-                    source={{uri: 'http://static.licaimofang.com/wp-content/uploads/2022/07/wx-icon.png'}}
-                    style={styles.wxImg}
-                />
-                <Text style={styles.copyText}>消息将通过“理财魔方”公众号发送</Text>
-                <TouchableOpacity activeOpacity={0.8} style={styles.copyBtn} onPress={copyToFollow}>
-                    <Text style={styles.copyBtnText}>复制去关注</Text>
-                </TouchableOpacity>
+                <FastImage source={{uri: data.header.line2.icon}} style={styles.wxImg} />
+                <Text style={styles.copyText}>{data.header.line2.text}</Text>
+                {data.header.line2?.button ? (
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={styles.copyBtn}
+                        onPress={() => {
+                            jump(data.header.line2?.button?.url);
+                        }}>
+                        <Text style={styles.copyBtnText}>{data.header.line2?.button.text}</Text>
+                    </TouchableOpacity>
+                ) : null}
             </View>
-            <View style={styles.switchCells}>
-                {[1, 2, 3].map((item, idx) => (
-                    <View key={idx} style={[styles.switchCell, idx > 0 ? styles.borderTop : {}]}>
-                        <View style={styles.switchCellLeft}>
-                            <Text style={styles.switchCellTitle}>全部直属</Text>
-                            <Text style={styles.switchCellDesc}>一键订阅</Text>
+            <View style={{marginTop: px(12)}} />
+            <ScrollView style={{flex: 1}} scrollIndicatorInsets={{right: 1}}>
+                <View>
+                    {data.list.map((item, idx) => (
+                        <View key={idx} style={[styles.switchCell, idx > 0 ? styles.borderTop : {}]}>
+                            <View style={styles.switchCellLeft}>
+                                <Text style={styles.switchCellTitle}>{item.index_name}</Text>
+                                <Text style={styles.switchCellDesc}>{item.desc}</Text>
+                            </View>
+                            <View>
+                                <Switch
+                                    ios_backgroundColor={'#CCD0DB'}
+                                    thumbColor={'#fff'}
+                                    trackColor={{false: '#CCD0DB', true: '#0051CC'}}
+                                    value={status[item.item_id]}
+                                    onValueChange={(state) => handlerCellSwitch(state, item.item_id, idx)}
+                                />
+                            </View>
                         </View>
-                        <View>
-                            <Switch
-                                ios_backgroundColor={'#CCD0DB'}
-                                thumbColor={'#fff'}
-                                trackColor={{false: '#CCD0DB', true: '#0051CC'}}
-                                value={true}
-                                onChange={handlerCellSwitch}
-                            />
-                        </View>
-                    </View>
-                ))}
-            </View>
-            <View style={{height: px(50)}} />
+                    ))}
+                </View>
+                <View style={{height: px(50)}} />
+            </ScrollView>
         </View>
-    );
+    ) : null;
 };
 
 export default SubscribeManage;
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        flex: 1,
+    },
     topHintWrap: {
         paddingVertical: px(12),
         paddingHorizontal: px(16),
@@ -109,9 +144,6 @@ const styles = StyleSheet.create({
         fontSize: px(12),
         lineHeight: px(17),
         color: '#fff',
-    },
-    switchCells: {
-        marginTop: px(12),
     },
     switchCell: {
         backgroundColor: '#fff',
