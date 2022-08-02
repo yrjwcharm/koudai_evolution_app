862e884d9a7a5a93bcf46b073350e8102f52dc7a
wxp <wangxuepeng@licaimofang.com>
2022-08-02 17:39:23 +0800

    sync

--- src/pages/FixedPortfolio/FixedUpdate.js
+++ src/pages/FixedPortfolio/FixedUpdate.js
@@ -303,6 +303,7 @@ export default function FixedUpdate({navigation, route}) {
                                 path: auto_charge?.button?.url?.path,
                                 params: {
                                     title: auto_charge?.button?.url?.params.title,
+                                    invest_id: auto_charge?.button?.url?.params?.invest_id,
                                     img: auto_charge?.detail_img,
                                 },
                             });
