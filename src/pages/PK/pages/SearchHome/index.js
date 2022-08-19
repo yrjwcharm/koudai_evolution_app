5595cb92ae3de6689ed35f003efaa149284de13d
wxp <wangxuepeng@licaimofang.com>
2022-08-19 10:28:47 +0800

    优化搜索页面样式

--- src/pages/PK/pages/SearchHome/index.js
+++ src/pages/PK/pages/SearchHome/index.js
@@ -105,7 +105,7 @@ const Index = () => {
                     searchLoading ? (
                         <LoadingTips size={px(18)} />
                     ) : searchList?.length > 0 ? (
-                        <View style={{marginBottom: px(40)}}>
+                        <View style={{marginBottom: px(110)}}>
                             {searchList?.map((item, key) => (
                                 <View key={key}>
                                     {item.title ? (
