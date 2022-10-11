eb349154ea9ac16101aac1df9f0d0d22fa5b4b5d
wxp <wangxuepeng@licaimofang.com>
2022-10-11 14:30:02 +0800

    PR-985:更多分类接口对接

--- src/pages/Common/MenuList.js
+++ src/pages/Common/MenuList.js
@@ -4,17 +4,18 @@
  * @Date: 2022-10-11 12:07:20
  */
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {useJump} from '~/components/hooks';
 import http from '~/services';
 import {px} from '~/utils/appUtil';
 
 const MenuList = ({navigation, route}) => {
+    const jump = useJump();
     const [data, setData] = useState();
 
     useEffect(() => {
-        http.get('/xxx', route.params).then((res) => {
-            console.log(res);
+        http.get('/products/menus/all/20220901', route.params).then((res) => {
             if (res.code === '000000') {
                 navigation.setOptions({title: res.result.title || '更多分类'});
                 setData(res.result);
@@ -26,11 +27,20 @@ const MenuList = ({navigation, route}) => {
         <View style={styles.container}>
             <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
                 <View style={styles.main}>
-                    {data.map((item, idx) => (
-                        <View style={styles.menuItem} key={idx}>
-                            <FastImage source={{uri: ''}} style={styles.menuItemIcon} />
-                            <Text style={styles.menuItemText}>123</Text>
-                        </View>
+                    {data?.menus?.map((item, idx) => (
+                        <TouchableOpacity
+                            style={styles.menuItem}
+                            key={idx}
+                            activeOpacity={0.8}
+                            onPress={() => {
+                                global.LogTool({
+                                    event: item.event_id,
+                                });
+                                jump(item.url);
+                            }}>
+                            <FastImage source={{uri: item.icon}} style={styles.menuItemIcon} />
+                            <Text style={styles.menuItemText}>{item.name}</Text>
+                        </TouchableOpacity>
                     ))}
                 </View>
             </ScrollView>
@@ -44,7 +54,6 @@ const styles = StyleSheet.create({
     container: {flex: 1, backgroundColor: '#fff'},
     main: {
         paddingHorizontal: px(19),
-        paddingVertical: px(24),
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -52,7 +61,8 @@ const styles = StyleSheet.create({
     },
     menuItem: {
         alignItems: 'center',
-        maxWidth: '20%',
+        width: '20%',
+        marginTop: px(24),
     },
     menuItemIcon: {
         width: px(26),
