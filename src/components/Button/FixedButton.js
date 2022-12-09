57c46b61bd267efb574c16c73016153a01cdce08
dengxiu <dengxiu@licaimofang.com>
2022-12-09 17:50:27 +0800

    PR-1109: 固定按钮转换为函数式组件

--- src/components/Button/FixedButton.js
+++ src/components/Button/FixedButton.js
@@ -1,101 +1,184 @@
 /*
  * @Date: 2021-01-06 18:39:56
- * @Author: yhc
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-09 17:29:02
  * @Description: 固定按钮
  */
-import React, {Component} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {StyleSheet, Keyboard, Animated, View, Text} from 'react-native';
 import Button from './Button';
 import Agreements from '../Agreements';
 import {px, isIphoneX, deviceWidth} from '../../utils/appUtil';
 import {Space} from '../../common/commonStyle';
-export default class FixedButton extends Component {
-    state = {
-        check: this.props.agreement?.default_agree !== undefined ? this.props.agreement?.default_agree : true,
-        keyboardHeight: new Animated.Value(0),
-        showCheckTag: true,
-    };
-    UNSAFE_componentWillMount() {
-        Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
-        Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
-    }
-    UNSAFE_componentWillReceiveProps(next) {
-        if (!this.agreement && next.agreement && this.props.agreement != next.agreement) {
-            this.setState({check: next.agreement.default_agree, showCheckTag: !next.agreement.default_agree});
-        }
-        this.agreement = next.agreement;
-    }
-    keyboardWillShow = (e) => {
-        const {keyboardHeight} = this.state;
+
+const FixedButton = (props) => {
+    const {
+        agreement,
+        disabled,
+        heightChange,
+        suffix = '',
+        otherAgreement,
+        otherParam,
+        checkIcon,
+        containerStyle,
+        agreementStyle,
+        ...restProps
+    } = props;
+    const keyboardHeight = useRef(new Animated.Value(0)).current;
+    const [check, setCheck] = useState();
+    const [showCheckTag, setShowCheckTag] = useState(true);
+    const firstIn = useRef(true);
+
+    const keyboardWillShow = (e) => {
         Animated.timing(keyboardHeight, {
             toValue: e.endCoordinates.height - (isIphoneX() ? 34 - px(8) : px(8)),
             duration: e.duration,
             useNativeDriver: false,
         }).start();
     };
-    keyboardWillHide = (e) => {
-        const {keyboardHeight} = this.state;
+    const keyboardWillHide = (e) => {
         Animated.timing(keyboardHeight, {
             toValue: 0,
             duration: e.duration,
             useNativeDriver: false,
         }).start();
     };
-    componentWillUnmount() {
-        Keyboard.removeListener('keyboardWillShow', this.keyboardWillShow);
-        Keyboard.removeListener('keyboardWillHide', this.keyboardWillHide);
-    }
-    render() {
-        const {check, keyboardHeight, showCheckTag} = this.state;
-        const {
-            agreement,
-            disabled,
-            heightChange,
-            suffix = '',
-            otherAgreement,
-            otherParam,
-            checkIcon,
-            containerStyle,
-            agreementStyle,
-        } = this.props;
-        return (
-            <Animated.View style={[styles.bottom, {bottom: keyboardHeight}, containerStyle]}>
-                {agreement?.radio_text && showCheckTag && !check ? (
-                    <View style={styles.checkTag}>
-                        <Text style={{fontSize: px(14), lineHeight: px(20), color: '#fff'}}>
-                            {agreement?.radio_text}
-                        </Text>
-                        <View style={styles.qualTag} />
-                    </View>
-                ) : null}
-                {agreement ? (
-                    <View
-                        style={[{paddingTop: px(4), paddingBottom: Space.padding}, agreementStyle]}
-                        onLayout={(e) => heightChange && heightChange(e.nativeEvent.layout.height)}>
-                        <Agreements
-                            check={agreement?.default_agree}
-                            data={agreement?.list}
-                            otherAgreement={otherAgreement}
-                            otherParam={otherParam}
-                            title={agreement?.text}
-                            text1={agreement?.text1}
-                            onChange={(checkStatus) => {
-                                global.LogTool({ctrl: checkStatus ? 'check' : 'uncheck', event: 'contract'});
-                                this.setState({check: checkStatus});
-                                this.setState({showCheckTag: !checkStatus});
-                            }}
-                            suffix={suffix}
-                            checkIcon={checkIcon}
-                        />
-                    </View>
-                ) : null}
-                <Button {...this.props} disabled={(agreement && !check) || disabled} />
-            </Animated.View>
-        );
-    }
-}
+
+    useEffect(() => {
+        Keyboard.addListener('keyboardWillShow', keyboardWillShow);
+        Keyboard.addListener('keyboardWillHide', keyboardWillHide);
+        return () => {
+            Keyboard.removeListener('keyboardWillShow', keyboardWillShow);
+            Keyboard.removeListener('keyboardWillHide', keyboardWillHide);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (firstIn.current && agreement) {
+            setCheck(agreement.default_agree);
+            setShowCheckTag(!agreement.default_agree);
+            firstIn.current = false;
+        }
+    }, [agreement]);
+
+    return (
+        <Animated.View style={[styles.bottom, {bottom: keyboardHeight}, containerStyle]}>
+            {agreement?.radio_text && showCheckTag && !check ? (
+                <View style={styles.checkTag}>
+                    <Text style={{fontSize: px(14), lineHeight: px(20), color: '#fff'}}>{agreement?.radio_text}</Text>
+                    <View style={styles.qualTag} />
+                </View>
+            ) : null}
+            {agreement ? (
+                <View
+                    style={[{paddingTop: px(4), paddingBottom: Space.padding}, agreementStyle]}
+                    onLayout={(e) => heightChange && heightChange(e.nativeEvent.layout.height)}>
+                    <Agreements
+                        check={agreement?.default_agree}
+                        data={agreement?.list}
+                        otherAgreement={otherAgreement}
+                        otherParam={otherParam}
+                        title={agreement?.text}
+                        text1={agreement?.text1}
+                        onChange={(checkStatus) => {
+                            global.LogTool({ctrl: checkStatus ? 'check' : 'uncheck', event: 'contract'});
+                            setCheck(checkStatus);
+                            setShowCheckTag(!checkStatus);
+                        }}
+                        suffix={suffix}
+                        checkIcon={checkIcon}
+                    />
+                </View>
+            ) : null}
+            <Button {...restProps} disabled={(agreement && !check) || disabled} />
+        </Animated.View>
+    );
+};
+
+export default FixedButton;
+// export default class FixedButton extends Component {
+//     state = {
+//         check: this.props.agreement?.default_agree !== undefined ? this.props.agreement?.default_agree : true,
+//         keyboardHeight: new Animated.Value(0),
+//         showCheckTag: true,
+//     };
+//     UNSAFE_componentWillMount() {
+//         Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
+//         Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
+//     }
+//     UNSAFE_componentWillReceiveProps(next) {
+//         if (!this.agreement && next.agreement && this.props.agreement != next.agreement) {
+//             this.setState({check: next.agreement.default_agree, showCheckTag: !next.agreement.default_agree});
+//         }
+//         this.agreement = next.agreement;
+//     }
+//     keyboardWillShow = (e) => {
+//         const {keyboardHeight} = this.state;
+//         Animated.timing(keyboardHeight, {
+//             toValue: e.endCoordinates.height - (isIphoneX() ? 34 - px(8) : px(8)),
+//             duration: e.duration,
+//             useNativeDriver: false,
+//         }).start();
+//     };
+//     keyboardWillHide = (e) => {
+//         const {keyboardHeight} = this.state;
+//         Animated.timing(keyboardHeight, {
+//             toValue: 0,
+//             duration: e.duration,
+//             useNativeDriver: false,
+//         }).start();
+//     };
+//     componentWillUnmount() {
+//         Keyboard.removeListener('keyboardWillShow', this.keyboardWillShow);
+//         Keyboard.removeListener('keyboardWillHide', this.keyboardWillHide);
+//     }
+//     render() {
+//         const {check, keyboardHeight, showCheckTag} = this.state;
+//         const {
+//             agreement,
+//             disabled,
+//             heightChange,
+//             suffix = '',
+//             otherAgreement,
+//             otherParam,
+//             checkIcon,
+//             containerStyle,
+//             agreementStyle,
+//         } = this.props;
+//         return (
+//             <Animated.View style={[styles.bottom, {bottom: keyboardHeight}, containerStyle]}>
+//                 {agreement?.radio_text && showCheckTag && !check ? (
+//                     <View style={styles.checkTag}>
+//                         <Text style={{fontSize: px(14), lineHeight: px(20), color: '#fff'}}>
+//                             {agreement?.radio_text}
+//                         </Text>
+//                         <View style={styles.qualTag} />
+//                     </View>
+//                 ) : null}
+//                 {agreement ? (
+//                     <View
+//                         style={[{paddingTop: px(4), paddingBottom: Space.padding}, agreementStyle]}
+//                         onLayout={(e) => heightChange && heightChange(e.nativeEvent.layout.height)}>
+//                         <Agreements
+//                             check={agreement?.default_agree}
+//                             data={agreement?.list}
+//                             otherAgreement={otherAgreement}
+//                             otherParam={otherParam}
+//                             title={agreement?.text}
+//                             text1={agreement?.text1}
+//                             onChange={(checkStatus) => {
+//                                 global.LogTool({ctrl: checkStatus ? 'check' : 'uncheck', event: 'contract'});
+//                                 this.setState({check: checkStatus});
+//                                 this.setState({showCheckTag: !checkStatus});
+//                             }}
+//                             suffix={suffix}
+//                             checkIcon={checkIcon}
+//                         />
+//                     </View>
+//                 ) : null}
+//                 <Button {...this.props} disabled={(agreement && !check) || disabled} />
+//             </Animated.View>
+//         );
+//     }
+// }
 const styles = StyleSheet.create({
     bottom: {
         backgroundColor: '#fff',
