bd8c3d81b13eed68132866f4e8ced9c12fe76252
wxp <wangxuepeng@licaimofang.com>
2022-12-26 16:21:48 +0800

    PR-1179: 优化部分细节

--- src/pages/Attention/OCRHome/services.js
+++ src/pages/Attention/OCRHome/services.js
@@ -2,13 +2,13 @@
  * @Date: 2022-06-27 15:33:32
  * @Author: yhc
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-23 16:42:08
+ * @LastEditTime: 2022-12-26 16:20:56
  * @Description:
  */
 import qs from 'qs';
 import http from '../../../services';
-export const getInfo = () => {
-    return http.get('/pic/upload/index/20220608');
+export const getInfo = (data) => {
+    return http.get('/pic/upload/index/20220608', data);
 };
 export const uploadFile = (data, progressCallBack, params) => {
     return http.uploadFiles(`/pic/upload/20220608${params ? '?' + qs.stringify(params) : ''}`, data, progressCallBack);
